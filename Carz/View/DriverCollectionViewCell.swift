fae64bc623de983b81eb9c22778482930636bc3a
Vincent <vinestro@me.com>
2022-03-09 17:16:04 +0100

    feature: Create the driver collection view cell

---
+++ Carz/View/DriverCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DriverCollectionViewCell.swift
+//  Carz
+//
+//  Created by Vincent DELMAESTRO on 09/03/2022.
+//
+
+import Foundation
+import UIKit
+
+class DriverCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+}
