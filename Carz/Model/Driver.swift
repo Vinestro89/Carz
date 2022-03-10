9c9720cf67eb21c0c46d43dba310a7d2f4832ff0
Vincent <vinestro@me.com>
2022-03-10 08:48:14 +0100

    refactor: remove unused hashable code

--- Carz/Model/Driver.swift
+++ Carz/Model/Driver.swift
@@ -8,17 +8,8 @@
 import Foundation
 
 struct Driver: Hashable {
-    let id: Int
     let team: Team
     let firstName: String
     let lastName: String
     let number: Int
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
-    static func == (lhs: Driver, rhs: Driver) -> Bool {
-      lhs.id == rhs.id
-    }
 }
--- Carz/Model/Team.swift
+++ Carz/Model/Team.swift
@@ -8,14 +8,5 @@
 import Foundation
 
 struct Team: Hashable {
-    let id: Int
     let name: String
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-    
-    static func == (lhs: Team, rhs: Team) -> Bool {
-      lhs.id == rhs.id
-    }
 }
